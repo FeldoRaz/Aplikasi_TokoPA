a342ece4b17b93f58f636a6a5f40fccdf7f8727d
feldo <feldorazzan@gmail.com>
2023-07-05 23:49:50 +0700

    ok

--- lib/keranjang.dart
+++ lib/keranjang.dart
@@ -5,7 +5,7 @@ class MyKeranjang extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       
-      appBar: AppBar(title: const Text('Keranjangku')),
+      appBar: AppBar(title: const Text('Keranjang')),
       body: Container(),
     );
   }
