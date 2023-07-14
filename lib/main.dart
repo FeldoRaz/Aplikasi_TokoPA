ebd60a60f172782bfb4cf6a044b6e2e9a1267fa0
feldo <feldorazzan@gmail.com>
2023-07-14 08:38:11 +0700

    strapi

--- lib/main.dart
+++ lib/main.dart
@@ -135,4 +135,43 @@ class _BerandaPageState extends State<BerandaPage> {
   }
 }
 
+class _BerandaPage extends State<BerandaPage> {
+  int _counter = 0;
 
+
+void _incrementCounter() {
+  setState(() {
+      _counter++;
+    });
+  }
+
+@override
+  Widget build(BuildContext context) {
+     return Scaffold(
+      appBar: AppBar(
+         backgroundColor: Theme.of(context).colorScheme.inversePrimary
+      ),
+      body: Center(
+        child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  
+}
